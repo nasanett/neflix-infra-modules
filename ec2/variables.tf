18ed389dde6f58cc4ee0985f7e7ca544755bcd12
nasanett <nasataku@yahoo.com>
2022-08-13 22:37:00 +0200

    adding common modules to repo

---
+++ ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+    default = "ami-051dfed8f67f095f5"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+
+variable "device_name" {
+    default = "nett"
+  
+}
+
+variable "sg_id" {
+    
+  
+}
